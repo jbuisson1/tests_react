176de6d4b8c70f25d1a3ab4d949be4c9de628927
jean-baotiste buisson <jbuisson1@gmail.com>
2017-07-10 08:52:24 +0200

    [Remove][Compiler and framework properties] Remove compiler and framework properties from the Hello class

--- project/src/components/Hello.tsx
+++ project/src/components/Hello.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 
-export interface HelloProps { compiler: string; framework: string; }
 
-
-export class Hello extends React.Component<HelloProps, any> {
+export class Hello extends React.Component<any, any> {
 
     constructor(props: any){
         super(props);
